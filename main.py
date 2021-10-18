feac7988fa0b9d995e468bbef598bd69a0a9ab26
SidMurthy97 <murthy.siddhant@googlemail.com>
2021-10-18 16:24:18 +0100

    data collection working, next to work on poster making automation

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from pprint import pprint
 
-lz_uri = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
+album_in = 'https://open.spotify.com/album/6OQ9gBfg5EXeNAEwGSs6jK?si=OivWWXdtTd6ea4v4mQpIhw' #album url 
 
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
-results = spotify.artist_top_tracks(lz_uri)
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials()) #spotify connection 
 
-for track in results['tracks'][:10]:
-    print('track    : ' + track['name'])
-    print('audio    : ' + track['preview_url'])
-    print('cover art: ' + track['album']['images'][0]['url'])
-    print()
+
+# data collection 
+results = spotify.album(album_in)
+n_tracks = results['total_tracks']
+cover_art = results['images'][0]['url']
+
+album_tracks = []
+for track in results['tracks']['items']:
+    album_tracks.append(track['name'])
+
+print(cover_art)
+print(album_tracks)
