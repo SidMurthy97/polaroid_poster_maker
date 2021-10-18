b2c089cb07057fda93440ca700e078e9549c17e2
SidMurthy97 <murthy.siddhant@googlemail.com>
2021-10-18 16:41:18 +0100

    added functionality for downloading 600x600 album art into a new named folder

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from pprint import pprint
+import requests
+import os 
 
 album_in = 'https://open.spotify.com/album/6OQ9gBfg5EXeNAEwGSs6jK?si=OivWWXdtTd6ea4v4mQpIhw' #album url 
 
@@ -9,6 +11,7 @@ spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
 # data collection 
 results = spotify.album(album_in)
+album_name = results['name']
 n_tracks = results['total_tracks']
 cover_art = results['images'][0]['url']
 
@@ -18,3 +21,13 @@ for track in results['tracks']['items']:
 
 print(cover_art)
 print(album_tracks)
+print(album_name)
+
+img_data = requests.get(cover_art).content
+
+filename = album_name + "/cover.png"
+os.makedirs(os.path.dirname(filename), exist_ok=True)
+
+with open(filename, 'wb') as handler:
+    handler.write(img_data)
+
